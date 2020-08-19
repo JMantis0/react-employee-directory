c7c3d526632817ec0e419e69653b5ac278f85193
JMan <60026263+JMantis0@users.noreply.github.com>
2020-08-19 10:03:09 -0700

    simplified sort functions and placed them directly into button onclicks using setEmployeestate.  Added a mock_data file with images

--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
 import EmployeeRow from "./components/EmployeeRow";
-import employees from "./employees";
+import employees from "./MOCK_DATA.json";
 function App() {
+  let [employeeState, setEmployeeState] = useState({
+    employees: employees,
+  });
+
   return (
     <div className="App">
       <Container fluid>
+        <button
+          onClick={() => {
+            setEmployeeState({
+              employees: employeeState.employees.sort((a, b) => {
+                return a.lastName < b.lastName ? -1 : 1;
+              }),
+            });
+          }}
+        >
+          Sort By Last Name (Ascending)
+        </button>
+        <button
+          onClick={() => {
+            setEmployeeState({
+              employees: employeeState.employees.sort((a, b) => {
+                return a.firstName < b.firstName ? -1 : 1;
+              }),
+            });
+          }}
+        >
+          Sort By First Name (Ascending)
+        </button>
+        <button
+          onClick={() => {
+            setEmployeeState({
+              employees: employeeState.employees.sort((a, b) => {
+                return a.lastName < b.lastName ? 1 : -1;
+              }),
+            });
+          }}
+        >
+          Sort By Last Name (Descending)
+        </button>
+        <button
+          onClick={() => {
+            setEmployeeState({
+              employees: employeeState.employees.sort((a, b) => {
+                return a.firstName < b.firstName ? 1 : -1;
+              }),
+            });
+          }}
+        >
+          Sort By First Name (Descending)
+        </button>
+
         <Header />
-        {employees.map((employee) => (
+        {employeeState.employees.map((employee) => (
           <EmployeeRow
+            thumbnail={employee.thumbnail}
             firstName={employee.firstName}
             lastName={employee.lastName}
             phone={employee.phone}
             email={employee.email}
+            key={Math.random()}
           />
         ))}
       </Container>
--- src/components/EmployeeRow.js
+++ src/components/EmployeeRow.js
@@ -2,10 +2,10 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function EmployeeRow ({ firstName, lastName, email, phone }) {
+function EmployeeRow ({ thumbnail, firstName, lastName, email, phone }) {
 
   return <Row>
-    <Col>Image</Col>
+    <Col><img style={{width: 50, height:50}} src={thumbnail}></img></Col>
     <Col>{firstName}</Col>
     <Col>{lastName}</Col>
     <Col>{email}</Col>
